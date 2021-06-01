f92aeddc0fe6f9e8f872566d8d00588cd2feb88f
amirrezamahmoudzadeh <81969097+amirrezamahmoudzadeh@users.noreply.github.com>
2021-06-01 18:45:12 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const bar = document.querySelector('.bar');
+const mainNav = document.querySelector('.main_nav ul');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+
+bar.addEventListener('click' , left);
+function left(){
+    mainNav.classList.toggle('right');
+    line1.classList.toggle('trans1');
+    line2.classList.toggle('trans2');
+    line3.classList.toggle('trans3');
+}

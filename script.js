dee818573d268506030f713c6b102d8adad2428b
amirrezamahmoudzadeh <81969097+amirrezamahmoudzadeh@users.noreply.github.com>
2021-06-07 00:33:19 +0430

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const line1 = document.querySelector('.line1');
 const line2 = document.querySelector('.line2');
 const line3 = document.querySelector('.line3');
 
-bar.addEventListener('click' , left);
-function left(){
-    mainNav.classList.toggle('right');
-    line1.classList.toggle('trans1');
-    line2.classList.toggle('trans2');
-    line3.classList.toggle('trans3');
-}
-window.addEventListener('mouseup' , function(Event){
-    if(Event.target !=mainNav && Event.target.parentNode !=mainNav){
+window.addEventListener('mouseup' , function hand(Event){
+    if(Event.target !=mainNav && Event.target.parentNode !=mainNav && Event.target.parentNode.parentNode !=mainNav){
         mainNav.classList.remove('right');
         line1.classList.remove('trans1');
         line2.classList.remove('trans2');
         line3.classList.remove('trans3');
+        bar.addEventListener('click' , left);
+            function left(){
+            mainNav.classList.toggle('right');
+            line1.classList.toggle('trans1');
+            line2.classList.toggle('trans2');
+            line3.classList.toggle('trans3');
+            }
     }
 });
+
